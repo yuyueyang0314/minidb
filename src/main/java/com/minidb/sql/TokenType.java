e100b87353ee411d98540cf9c07adef444c4dcac
ljzaizheli <150006378+ljzaizheli@users.noreply.github.com>
2025-09-14 18:08:53 +0800

    Add files via upload

---
+++ src/main/java/com/minidb/sql/TokenType.java
@@ -0,0 +1,6 @@
+package com.minidb.sql;
+public enum TokenType {
+    IDENT, NUMBER, STRING, COMMA, LPAREN, RPAREN, STAR,
+    CREATE, TABLE, INSERT, INTO, VALUES, SELECT, FROM, WHERE,
+    INT, TEXT, EQ, LT, GT, SEMI, EOF
+}
